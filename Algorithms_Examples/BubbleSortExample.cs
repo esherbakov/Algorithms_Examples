6ab0f8d5bdda6786d35c1f390b31c8f380d65c61
drows <drows@DROPIANPC>
2024-01-24 17:03:23 +0300

    Добавьте файлы проекта.

---
+++ Algorithms_Examples/BubbleSortExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Examples
+{
+    public class BubbleSortExample
+    {
+        public int[] BubbleSort(int[] mas)
+        {
+            if (mas.Length < 2)
+                return mas;
+            int temp = new int();
+            for (int i = 1; i < mas.Length; i++)
+                for (int j = 0; j < mas.Length - i; j++)
+                {
+                    if (mas[j] > mas[j + 1])
+                    {
+                        temp = mas[j];
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = temp;
+                    }
+                }
+            return mas;
+        }
+
+        public int[] BubbleSortDesc(int[] mas)
+        {
+            if (mas.Length < 2)
+                return mas;
+            int temp = new int();
+            for (int i = 1; i < mas.Length; i++)
+                for (int j = 0; j < mas.Length - i; j++)
+                {
+                    if (mas[j] < mas[j + 1])
+                    {
+                        temp = mas[j];
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = temp;
+                    }
+                }
+            return mas;
+        }
+    }
+}
---
+++ Algorithms_Examples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSortExample bubbleSort = new BubbleSortExample();
+            int[] mas = { 1, 5, 3, 4, 2 };
+            foreach (int item in bubbleSort.BubbleSort(mas))
+                Console.Write($"{item}, ");
+            Console.WriteLine();
+            foreach (int item in bubbleSort.BubbleSortDesc(mas))
+                Console.Write($"{item}, ");
+            Console.ReadKey();
+        }
+    }
+}
